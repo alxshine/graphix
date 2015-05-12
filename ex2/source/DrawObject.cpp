88e3268541d72f4cb26e58aa9c31fce6664d13a3
alxshine <a.schloegl0x5e@gmail.com>
2015-05-12 18:49:34 +0200

    prepared second exercise, everything compiles

---
+++ ex2/source/DrawObject.cpp
@@ -0,0 +1,48 @@
+#include "DrawObject.hpp"
+
+DrawObject::DrawObject(obj_scene_data *data){
+	v_size = data->vertex_count;
+	i_size = data->face_count;
+	
+	vertices = (GLfloat*) calloc(v_size*3, sizeof(GLfloat));
+	indices = (GLushort*) calloc(i_size*3, sizeof(GLushort));
+	
+	//copy vertices
+	for(int i = 0; i<v_size; i++){
+		vertices[i*3] = (GLfloat) (*data->vertex_list[i]).e[0];
+		vertices[i*3+1] = (GLfloat) (*data->vertex_list[i]).e[1];
+		vertices[i*3+2] = (GLfloat) (*data->vertex_list[i]).e[2];
+	}
+	
+	//copy indices
+	for(int i=0; i<i_size; i++){
+		indices[i*3] = (GLushort) (*data->face_list[i]).vertex_index[0];
+		indices[i*3+1] = (GLushort) (*data->face_list[i]).vertex_index[1];
+		indices[i*3+2] = (GLushort) (*data->face_list[i]).vertex_index[2];
+	}
+	
+	setupDataBuffers();
+}
+
+DrawObject::DrawObject(GLfloat *vertex_array, int vertex_size, GLushort *index_array, int index_size){
+	v_size = vertex_size;
+	i_size = index_size;
+	
+	vertices = (GLfloat*) calloc(v_size*3, sizeof(GLfloat));
+	indices = (GLushort*) calloc(i_size*3, sizeof(GLushort));
+	
+	memcpy(vertices, vertex_array, 3*vertex_size*sizeof(GLfloat));
+	memcpy(indices, index_array, 3*index_size*sizeof(GLushort));
+	
+	setupDataBuffers();
+}
+
+void DrawObject::setupDataBuffers(){
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, v_size*3*sizeof(GLfloat), vertices, GL_STATIC_DRAW);
+	
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ARRAY_BUFFER, i_size*3*sizeof(GLushort), indices, GL_STATIC_DRAW);
+}
---
+++ ex2/source/DrawObject.hpp
@@ -0,0 +1,28 @@
+#ifndef dObject
+#define dObject
+
+//include GL stuff
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+#include "OBJParser.h"
+
+class DrawObject{		
+	private:
+		void setupDataBuffers();
+	
+	public:
+		GLuint vbo, ibo;
+		float InitialTransform[16];
+		
+		GLfloat *vertices;
+		GLushort *indices;
+		int v_size, i_size;
+		
+		enum DataID{vPosition = 0, vColor = 1};
+		DrawObject(obj_scene_data* data);
+		DrawObject(GLfloat* vertex_array, int vertex_size, GLushort* index_array, int index_size);
+		~DrawObject();
+};
+
+#endif
